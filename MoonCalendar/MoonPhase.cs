27ae04121165ec4e34a1721897a4c80d7a35088f
Дмитрий Березиков <dm.berezikov@yandex.ru>
2024-01-22 00:23:24 +0700

    Частично описаны свойства класса

--- MoonCalendar/MoonPhase.cs
+++ MoonCalendar/MoonPhase.cs
@@ -2,9 +2,15 @@
 {
     public class MoonPhase
     {
-        private int _currentDay;
-        private int _currentMonth;
-        private int _currentYear;
+        private readonly int _currentDay;
+        private readonly int _currentMonth;
+        private readonly int _currentYear;
+
+        public double MoonAge { get; private set; }
+        public double MoonDistance { get; private set; } // Moon's distance in earth radius
+        public double MoonLatitude { get; private set; } // Moon's ecliptic latitude
+        public double MoonLongitude { get; private set; } // Moon's ecliptic longitude
+        public string Phase { get; private set; }
 
         public MoonPhase() : this (DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day) { }
 
@@ -13,29 +19,28 @@
             _currentYear = year;
             _currentMonth = month;
             _currentDay = day;
+
+            Calculate();
         }
 
         public static MoonPhase Create(int year, int month, int day)
         {
-            if (!DateTime.TryParse($"{day}.{month}.{year}", out _)) throw new ArgumentException("Неверная дата");
+            if (!DateTime.TryParse($"{day}.{month}.{year}", out _)) throw new ArgumentException("Не верная дата");
+
             return new MoonPhase(year, month, day);
         }
 
 
-        private void MoonPosit(int year, int month, int day)
+        private void Calculate()
         {
-
-            double ag; // Moon's age
-            double di; // Moon's distance in earth radius
-            double la; // Moon's ecliptic latitude
-            double lo; // Moon's ecliptic longitude
-            string phase;
+            
+          
             int yy, mm, k1, k2, k3, jd;
             double ip, dp, np, rp;
 
             //Calculate the Julian date at 12h UT
-            yy = year - (int)Math.Floor((12.0 - month) / 10.0);
-            mm = month + 9;
+            yy = _currentYear - (int)Math.Floor((12.0 - _currentMonth) / 10.0);
+            mm = _currentMonth + 9;
             if (mm >= 12)
             {
                 mm = mm - 12;
@@ -43,29 +48,29 @@
             k1 = (int)Math.Floor(365.25 * (yy + 4712.0));
             k2 = (int)Math.Floor(30.6 * mm + 0.5);
             k3 = (int)Math.Floor(((yy / 100.0) + 49.0) * 0.75) - 38;
-            jd = k1 + k2 + day + 59;
+            jd = k1 + k2 + _currentDay + 59;
             if (jd > 2299160)
             {
                 jd = jd - k3;
             }
             //Calculate moon's age in days
             ip = Normalize((jd - 2451550.1) / 29.530588853);
-            ag = ip * 29.53;
-            phase = MoonPhaseCalc(ag);
+            MoonAge = ip * 29.53;
+            Phase = MoonPhaseCalc(MoonAge);
             ip = ip * 2 * Math.PI;
             //Calculate moon's distance
             dp = 2 * Math.PI * Normalize((jd - 2451562.2) / 27.55454988);
-            di = 60.4 - 3.3 * Math.Cos(dp) - 0.6 * Math.Cos(2 * ip - dp) - 0.5 * Math.Cos(2 * ip);
+            MoonDistance = 60.4 - 3.3 * Math.Cos(dp) - 0.6 * Math.Cos(2 * ip - dp) - 0.5 * Math.Cos(2 * ip);
 
             //Calculate moon's ecliptic latitude
             np = 2 * Math.PI * Normalize((jd - 2451565.2) / 27.212220817);
-            la = 5.1 * Math.Sin(np);
+            MoonLatitude = 5.1 * Math.Sin(np);
             //calculate moon's ecliptic longitude
             rp = Normalize((jd - 2451555.8) / 27.321582241);
-            lo = 360 * rp + 6.3 * Math.Sin(dp) + 1.3 * Math.Sin(2 * ip - dp) + 0.7 * Math.Sin(2 * ip);
+            MoonLongitude = 360 * rp + 6.3 * Math.Sin(dp) + 1.3 * Math.Sin(2 * ip - dp) + 0.7 * Math.Sin(2 * ip);
             //display results
-            print(phase);
-            print(ag);
+            print(Phase);
+            print(MoonAge);
             //Cprint("distance = {0:F2} Earth radius", di);
             //print("Ecliptic:");
             //print("Latitude = {0:F2}", la);
@@ -74,45 +79,45 @@
         }
 
 
-        string MoonPhaseCalc(double ag)
+        string MoonPhaseCalc(double moonsAge)
         {
             string phase;
-            if (ag < 1.84566)
+            if (moonsAge < 1.84566)
             {
                 phase = "Новая Луна";
                 return phase;
             }
-            if (ag < 5.53699)
+            if (moonsAge < 5.53699)
             {
                 phase = "Waxing cresent";
                 return phase;
             }
-            if (ag < 9.22831)
+            if (moonsAge < 9.22831)
             {
                 phase = "First quarter";
                 return phase;
             }
-            if (ag < 12.91963)
+            if (moonsAge < 12.91963)
             {
                 phase = "Waxing gibbous";
                 return phase;
             }
-            if (ag < 16.61096)
+            if (moonsAge < 16.61096)
             {
                 phase = "Полная луна";
                 return phase;
             }
-            if (ag < 20.30228)
+            if (moonsAge < 20.30228)
             {
                 phase = "Wanning gibbous";
                 return phase;
             }
-            if (ag < 23.99361)
+            if (moonsAge < 23.99361)
             {
                 phase = "Last quarter";
                 return phase;
             }
-            if (ag < 27.68493)
+            if (moonsAge < 27.68493)
             {
                 phase = "Wanning crescent";
                 return phase;
