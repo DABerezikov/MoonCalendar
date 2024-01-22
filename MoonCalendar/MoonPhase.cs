301ef0002aa80478001b2a1689d879e7784a913b
Дмитрий Березиков <dm.berezikov@yandex.ru>
2024-01-23 00:34:32 +0700

    Доделаны расчеты

--- MoonCalendar/MoonPhase.cs
+++ MoonCalendar/MoonPhase.cs
@@ -6,7 +6,15 @@
         private readonly int _currentMonth;
         private readonly int _currentYear;
 
-        public double MoonAge { get; private set; }
+        private int _julianYear;
+        private int _julianMonth;
+        private int _julianDay;
+
+        private int _factor1;
+        private int _factor2;
+        private int _factor3;
+
+        public double MoonAge { get; }
         public double MoonDistance { get; private set; } // Moon's distance in earth radius
         public double MoonLatitude { get; private set; } // Moon's ecliptic latitude
         public double MoonLongitude { get; private set; } // Moon's ecliptic longitude
@@ -19,13 +27,14 @@
             _currentYear = year;
             _currentMonth = month;
             _currentDay = day;
-
             Calculate();
+            MoonAge = CalculateMoonAge();
+            Phase = MoonPhaseCalc(MoonAge);
         }
 
         public static MoonPhase Create(int year, int month, int day)
         {
-            if (!DateTime.TryParse($"{day}.{month}.{year}", out _)) throw new ArgumentException("Не верная дата");
+            if (!DateTime.TryParse($"{day}.{month}.{year}", out _)) throw new ArgumentException("Неверная дата");
 
             return new MoonPhase(year, month, day);
         }
@@ -33,106 +42,91 @@
 
         private void Calculate()
         {
+            CalculateJulianDate();
+
+            var synodicMonthRadian = Normalize((_julianDay - 2451550.1) / 29.530588853) * 2 * Math.PI;
+            var anomalyMonthRadian = 2 * Math.PI * Normalize((_julianDay - 2451562.2) / 27.55454988);
+            var draconicMonthRadian = 2 * Math.PI * Normalize((_julianDay - 2451565.2) / 27.212220817);
+            var tropicalMonthRadian = Normalize((_julianDay - 2451555.8) / 27.321582241);
             
-          
-            int yy, mm, k1, k2, k3, jd;
-            double ip, dp, np, rp;
+            MoonDistance = 60.4 - 3.3 * Math.Cos(anomalyMonthRadian) - 0.6 * Math.Cos(2 * synodicMonthRadian - anomalyMonthRadian) - 0.5 * Math.Cos(2 * synodicMonthRadian);
+            MoonLatitude = 5.1 * Math.Sin(draconicMonthRadian);
+            MoonLongitude = 360 * tropicalMonthRadian + 6.3 * Math.Sin(anomalyMonthRadian) + 1.3 * Math.Sin(2 * synodicMonthRadian - anomalyMonthRadian) + 0.7 * Math.Sin(2 * synodicMonthRadian);
+           
+        }
 
-            //Calculate the Julian date at 12h UT
-            yy = _currentYear - (int)Math.Floor((12.0 - _currentMonth) / 10.0);
-            mm = _currentMonth + 9;
-            if (mm >= 12)
-            {
-                mm = mm - 12;
-            }
-            k1 = (int)Math.Floor(365.25 * (yy + 4712.0));
-            k2 = (int)Math.Floor(30.6 * mm + 0.5);
-            k3 = (int)Math.Floor(((yy / 100.0) + 49.0) * 0.75) - 38;
-            jd = k1 + k2 + _currentDay + 59;
-            if (jd > 2299160)
-            {
-                jd = jd - k3;
-            }
-            //Calculate moon's age in days
-            ip = Normalize((jd - 2451550.1) / 29.530588853);
-            MoonAge = ip * 29.53;
-            Phase = MoonPhaseCalc(MoonAge);
-            ip = ip * 2 * Math.PI;
-            //Calculate moon's distance
-            dp = 2 * Math.PI * Normalize((jd - 2451562.2) / 27.55454988);
-            MoonDistance = 60.4 - 3.3 * Math.Cos(dp) - 0.6 * Math.Cos(2 * ip - dp) - 0.5 * Math.Cos(2 * ip);
+        private double CalculateMoonAge() => Normalize((_julianDay - 2451550.1) / 29.530588853) * 29.53;
+            
+        private void CalculateJulianDate()
+        {
+            _julianYear = _currentYear - (int)Math.Floor((12.0 - _currentMonth) / 10.0);
+            _julianMonth = _currentMonth + 9;
+            if (_julianMonth >= 12)
+                _julianMonth -= 12;
 
-            //Calculate moon's ecliptic latitude
-            np = 2 * Math.PI * Normalize((jd - 2451565.2) / 27.212220817);
-            MoonLatitude = 5.1 * Math.Sin(np);
-            //calculate moon's ecliptic longitude
-            rp = Normalize((jd - 2451555.8) / 27.321582241);
-            MoonLongitude = 360 * rp + 6.3 * Math.Sin(dp) + 1.3 * Math.Sin(2 * ip - dp) + 0.7 * Math.Sin(2 * ip);
-            //display results
-            print(Phase);
-            print(MoonAge);
-            //Cprint("distance = {0:F2} Earth radius", di);
-            //print("Ecliptic:");
-            //print("Latitude = {0:F2}", la);
-            //print("Longitude = {0:F2}", lo);
-            Console.ReadLine();
+            _factor1 = (int)Math.Floor(365.25 * (_julianYear + 4712.0));
+            _factor2 = (int)Math.Floor(30.6 * _julianMonth + 0.5);
+            _factor3 = (int)Math.Floor(((_julianYear / 100.0) + 49.0) * 0.75) - 38;
+            _julianDay = _factor1 + _factor2 + _currentDay + 59;
+            if (_julianDay > 2299160)
+                _julianDay -= _factor3;
         }
 
 
-        string MoonPhaseCalc(double moonsAge)
+        private static string MoonPhaseCalc(double moonsAge)
         {
             string phase;
             if (moonsAge < 1.84566)
             {
-                phase = "Новая Луна";
+                phase = "Новолуние";
                 return phase;
             }
             if (moonsAge < 5.53699)
             {
-                phase = "Waxing cresent";
+                phase = "Растущий серп";
                 return phase;
             }
             if (moonsAge < 9.22831)
             {
-                phase = "First quarter";
+                phase = "Первая четверть";
                 return phase;
             }
             if (moonsAge < 12.91963)
             {
-                phase = "Waxing gibbous";
+                phase = "Растущая луна";
                 return phase;
             }
             if (moonsAge < 16.61096)
             {
-                phase = "Полная луна";
+                phase = "Полнолуние";
                 return phase;
             }
             if (moonsAge < 20.30228)
             {
-                phase = "Wanning gibbous";
+                phase = "Убывающая луна";
                 return phase;
             }
             if (moonsAge < 23.99361)
             {
-                phase = "Last quarter";
+                phase = "Третья четверть";
                 return phase;
             }
             if (moonsAge < 27.68493)
             {
-                phase = "Wanning crescent";
+                phase = "Убывающий месяц";
                 return phase;
             }
-            phase = "Новая Луна";
+            phase = "Новолуние";
             return phase;
         }
 
-        static double Normalize(double v)
+        private static double Normalize(double v)
         {
-            v = v - Math.Floor(v);
+            v -= Math.Floor(v);
+
             if (v < 0)
-            {
-                v = v + 1;
-            }
+                v += 1;
+            
             return v;
         }
 
