25687d8f4f792b3e9b575f7bfd9b6a4e17724076
Дмитрий Березиков <dm.berezikov@yandex.ru>
2024-02-23 13:59:20 +0700

    Полностью изменена логика работы класса. По умолчанию отдает текущие значения, добавлены методы для получения значений по заданной дате

--- MoonCalendar/MoonPhase.cs
+++ MoonCalendar/MoonPhase.cs
@@ -2,69 +2,113 @@
 
 public class MoonPhase
 {
-    private readonly int _currentDay;
-    private readonly int _currentMonth;
-    private readonly int _currentYear;
-
-    private int _factor1;
-    private int _factor2;
-    private int _factor3;
-    private int _julianDay;
-    private int _julianMonth;
-
-    private int _julianYear;
-
-    public MoonPhase() : this(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day)
-    {
+    /// <summary> Текущий лунный день </summary>
+    public static double MoonAge {
+        get
+        {
+            var julianDay = CalculateJulianDay(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+            return CalculateMoonAge(julianDay);
+        }
     }
 
-    private MoonPhase(int year, int month, int day)
+    /// <summary> Текущая фаза Луны </summary>
+    public string LunarPhase => MoonPhaseCalc(MoonAge);
+
+    /// <summary> Текущее расстояние до Луны </summary>
+    public double MoonDistance => GetMoonDistance(DateTime.Now); // Moon's distance in earth radius
+    
+    /// <summary> Текущая широта затмения Луны </summary>
+    public double MoonLatitude => GetMoonLatitude(DateTime.Now);// Moon's ecliptic latitude
+    
+    /// <summary> Текущая долгота затмения Луны </summary>
+    public double MoonLongitude => GetMoonLongitude(DateTime.Now); // Moon's ecliptic longitude
+    
+    /// <summary> Текущее созвездие </summary>
+    public string Zodiac => GetZodiac(MoonLongitude);
+
+    /// <summary>Лунный день в указанную дату </summary>
+    /// <param name="date">Интересующая дата</param>
+    /// <returns>Лунный день</returns>
+    public double GetMoonAge(DateTime date)
     {
-        _currentYear = year;
-        _currentMonth = month;
-        _currentDay = day;
-        Calculate();
-        MoonAge = CalculateMoonAge();
-        Phase = MoonPhaseCalc(MoonAge);
-        Zodiac = GetZodiac();
+        var julianDay = CalculateJulianDay(date.Year, date.Month, date.Day);
+        return CalculateMoonAge(julianDay);
     }
 
-    public double MoonAge { get; }
-    public double MoonDistance { get; private set; } // Moon's distance in earth radius
-    public double MoonLatitude { get; private set; } // Moon's ecliptic latitude
-    public double MoonLongitude { get; private set; } // Moon's ecliptic longitude
-    public string Phase { get; private set; }
-    public string Zodiac { get; private set; }
-
-    public static MoonPhase Create(int year, int month, int day)
+    /// <summary>Фаза Луны в указанную дату </summary>
+    /// <param name="date">Интересующая дата</param>
+    /// <returns>Фаза луны</returns>
+    public string GetMoonPhase(DateTime date)
     {
-        if (!DateTime.TryParse($"{day}.{month}.{year}", out _)) throw new ArgumentException("Неверная дата");
-
-        return new MoonPhase(year, month, day);
+        var moonAge = GetMoonAge(date);
+        return MoonPhaseCalc(moonAge);
     }
 
-
-    private void Calculate()
+    /// <summary>Расстояние до Луны в указанную дату</summary>
+    /// <param name="date">Интересующая дата</param>
+    /// <returns>Расстояние до Луны</returns>
+    public double GetMoonDistance(DateTime date)
     {
-        CalculateJulianDate();
+        var year = date.Year;
+        var month = date.Month;
+        var day = date.Day;
+        var julianDay = CalculateJulianDay(year, month, day);
 
-        var synodicMonthRadian = Normalize((_julianDay - 2451550.1) / 29.530588853) * 2 * Math.PI;
-        var anomalyMonthRadian = 2 * Math.PI * Normalize((_julianDay - 2451562.2) / 27.55454988);
-        var draconicMonthRadian = 2 * Math.PI * Normalize((_julianDay - 2451565.2) / 27.212220817);
-        var tropicalMonthRadian = Normalize((_julianDay - 2451555.8) / 27.321582241);
+        var synodicMonthRadian = Normalize((julianDay - 2451550.1) / 29.530588853) * 2 * Math.PI;
+        var anomalyMonthRadian = 2 * Math.PI * Normalize((julianDay - 2451562.2) / 27.55454988);
 
-        MoonDistance = 60.4 - 3.3 * Math.Cos(anomalyMonthRadian) -
-                       0.6 * Math.Cos(2 * synodicMonthRadian - anomalyMonthRadian) -
-                       0.5 * Math.Cos(2 * synodicMonthRadian);
-        MoonLatitude = 5.1 * Math.Sin(draconicMonthRadian);
-        MoonLongitude = 360 * tropicalMonthRadian + 6.3 * Math.Sin(anomalyMonthRadian) +
-                        1.3 * Math.Sin(2 * synodicMonthRadian - anomalyMonthRadian) +
-                        0.7 * Math.Sin(2 * synodicMonthRadian);
+        return 60.4 - 3.3 * Math.Cos(anomalyMonthRadian) -
+               0.6 * Math.Cos(2 * synodicMonthRadian - anomalyMonthRadian) -
+               0.5 * Math.Cos(2 * synodicMonthRadian);
+        
     }
 
-    private string GetZodiac()
+    /// <summary>Широта затмения Луны в указанную дату</summary>
+    /// <param name="date">Интересующая дата</param>
+    /// <returns>Широта затмения</returns>
+    public double GetMoonLatitude(DateTime date)
     {
-        return MoonLongitude switch
+        var year = date.Year;
+        var month = date.Month;
+        var day = date.Day;
+        var julianDay = CalculateJulianDay(year, month, day);
+        var draconicMonthRadian = 2 * Math.PI * Normalize((julianDay - 2451565.2) / 27.212220817);
+        return 5.1 * Math.Sin(draconicMonthRadian);
+        
+    }
+
+    /// <summary>Долгота затмения Луны в указанную дату</summary>
+    /// <param name="date">Интересующая дата</param>
+    /// <returns>Долгота затмения</returns>
+    public double GetMoonLongitude(DateTime date)
+    {
+        var year = date.Year;
+        var month = date.Month;
+        var day = date.Day;
+        var julianDay = CalculateJulianDay(year, month, day);
+
+        var synodicMonthRadian = Normalize((julianDay - 2451550.1) / 29.530588853) * 2 * Math.PI;
+        var anomalyMonthRadian = 2 * Math.PI * Normalize((julianDay - 2451562.2) / 27.55454988);
+        var tropicalMonthRadian = Normalize((julianDay - 2451555.8) / 27.321582241);
+
+        return 360 * tropicalMonthRadian + 6.3 * Math.Sin(anomalyMonthRadian) +
+               1.3 * Math.Sin(2 * synodicMonthRadian - anomalyMonthRadian) +
+               0.7 * Math.Sin(2 * synodicMonthRadian);
+    }
+
+    /// <summary>Знак зодиака Луны в указанную дату</summary>
+    /// <param name="date">Интересующая дата</param>
+    /// <returns>Знак зодиака</returns>
+    public string GetZodiac(DateTime date)
+    {
+        var moonLongitude = GetMoonLongitude(date);
+        return GetZodiac(moonLongitude);
+    }
+
+    private string GetZodiac(double moonLongitude)
+    {
+        
+        return moonLongitude switch
         {
             < 33.18 => "Рыбы",
             < 51.16 => "Овен",
@@ -82,24 +126,25 @@ public class MoonPhase
         };
     }
 
-    private double CalculateMoonAge()
+    private static double CalculateMoonAge(int julianDay)
     {
-        return Normalize((_julianDay - 2451550.1) / 29.530588853) * 29.53;
+        return Normalize((julianDay - 2451550.1) / 29.530588853) * 29.53;
     }
 
-    private void CalculateJulianDate()
+    private static int CalculateJulianDay(int year, int month, int day)
     {
-        _julianYear = _currentYear - (int)Math.Floor((12.0 - _currentMonth) / 10.0);
-        _julianMonth = _currentMonth + 9;
-        if (_julianMonth >= 12)
-            _julianMonth -= 12;
+        var julianYear = year - (int)Math.Floor((12.0 - month) / 10.0);
+        var julianMonth = month + 9;
+        if (julianMonth >= 12)
+            julianMonth -= 12;
 
-        _factor1 = (int)Math.Floor(365.25 * (_julianYear + 4712.0));
-        _factor2 = (int)Math.Floor(30.6 * _julianMonth + 0.5);
-        _factor3 = (int)Math.Floor((_julianYear / 100.0 + 49.0) * 0.75) - 38;
-        _julianDay = _factor1 + _factor2 + _currentDay + 59;
-        if (_julianDay > 2299160)
-            _julianDay -= _factor3;
+        var factor1 = (int)Math.Floor(365.25 * (julianYear + 4712.0));
+        var factor2 = (int)Math.Floor(30.6 * julianMonth + 0.5);
+        var factor3 = (int)Math.Floor((julianYear / 100.0 + 49.0) * 0.75) - 38;
+        var julianDay = factor1 + factor2 + day + 59;
+        if (julianDay > 2299160)
+            julianDay -= factor3;
+        return julianDay;
     }
 
 
